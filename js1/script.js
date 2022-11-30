ee69fa0234282fb1b822c5f50170eb24a3ac6419
DianaMajlumova <116622626+DianaMajlumova@users.noreply.github.com>
2022-11-30 20:06:13 +0400

    Add files via upload

---
+++ js1/script.js
@@ -0,0 +1,6 @@
+// task I
+
+const arr = [1, 2, 3, 4, 5];
+const average = arr.reduce((a, b) => a + b, 0) / arr.length;
+
+console.log(average);
---
+++ js2/script.js
@@ -0,0 +1,13 @@
+// let string=["Necesen"]
+//  for (let i = 0; i < string.length; i++) {
+//     if ("n") {
+//     count ++
+//    console.log(count);
+   
+//  }}
+
+//  task II
+
+const fruits = [1,5,2,7];
+fruits.push(11);
+console.log(fruits);
